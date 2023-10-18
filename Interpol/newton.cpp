1071288c2930e39e0b1dbead7f5eb7bd4e4dd044
KateLy <91429213+KateLy1@users.noreply.github.com>
2023-10-18 08:34:45 +0300

    Update newton.cpp

--- Interpol/newton.cpp
+++ Interpol/newton.cpp
@@ -3,59 +3,59 @@ using namespace std;
 
 int main()
 {
-    // организация потока вывода cout в файл csv
+    // РѕСЂРіР°РЅРёР·Р°С†РёСЏ РїРѕС‚РѕРєР° РІС‹РІРѕРґР° cout РІ С„Р°Р№Р» csv
     std::ofstream out("newton_interpolation.csv");
     std::streambuf *coutbuf = std::cout.rdbuf(); //save old buf
     std::cout.rdbuf(out.rdbuf());               //redirect std::cout to file
     std::ofstream output;
 
 //    const std::array<int, 3> N_nodes{3, 4, 5};
-    double a = 0.0, b = 2.0, bt, err;          // отрезок интерполяции [a;b]
+    double a = 0.0, b = 2.0, bt, err;          // РѕС‚СЂРµР·РѕРє РёРЅС‚РµСЂРїРѕР»СЏС†РёРё [a;b]
 
 
-// Вычисление ошибки интерполяции для 3, 4 или 5 узлов интерполяции
-// на последовательности отрезков [0,2], [0,1], [0,1/2], ... [0, 1/16].
-// Равномерное расположение узлов.
+// Р’С‹С‡РёСЃР»РµРЅРёРµ РѕС€РёР±РєРё РёРЅС‚РµСЂРїРѕР»СЏС†РёРё РґР»СЏ 3, 4 РёР»Рё 5 СѓР·Р»РѕРІ РёРЅС‚РµСЂРїРѕР»СЏС†РёРё
+// РЅР° РїРѕСЃР»РµРґРѕРІР°С‚РµР»СЊРЅРѕСЃС‚Рё РѕС‚СЂРµР·РєРѕРІ [0,2], [0,1], [0,1/2], ... [0, 1/16].
+// Р Р°РІРЅРѕРјРµСЂРЅРѕРµ СЂР°СЃРїРѕР»РѕР¶РµРЅРёРµ СѓР·Р»РѕРІ.
     bool Cheb_nodes = false;
     bt = b;
-    for (int bk = 0; bk<6; bk++){           // цикл по отрезкам [a;b] (b меняется от 2 до 1/16)
+    for (int bk = 0; bk<6; bk++){           // С†РёРєР» РїРѕ РѕС‚СЂРµР·РєР°Рј [a;b] (b РјРµРЅСЏРµС‚СЃСЏ РѕС‚ 2 РґРѕ 1/16)
         err = Newton_interpolator_err<double, double, 3>(a, bt, Cheb_nodes);
-        std::cout << "3" << ";" << bt << ";" << err << ";" << "Равн" << std::endl;
+        std::cout << "3" << ";" << bt << ";" << err << ";" << "Р Р°РІРЅ" << std::endl;
         bt = bt/2.0;
     }
     bt = b;
     for (int bk = 0; bk<6; bk++){
         err = Newton_interpolator_err<double, double, 4>(a, bt, Cheb_nodes);
-        std::cout << "4" << ";" << bt << ";" << err << ";" << "Равн" << std::endl;
+        std::cout << "4" << ";" << bt << ";" << err << ";" << "Р Р°РІРЅ" << std::endl;
         bt = bt/2.0;
     }
     bt = b;
     for (int bk = 0; bk<6; bk++){
         err = Newton_interpolator_err<double, double, 5>(a, bt, Cheb_nodes);
-        std::cout << "5" << ";" << bt << ";" << err << ";" << "Равн" << std::endl;
+        std::cout << "5" << ";" << bt << ";" << err << ";" << "Р Р°РІРЅ" << std::endl;
         bt = bt/2.0;
     }
 
-// Вычисление ошибки интерполяции для 3, 4 или 5 узлов интерполяции
-// на последовательности отрезков [0,2], [0,1], [0,1/2], ... [0, 1/16].
-// Чебышевское расположение узлов.
+// Р’С‹С‡РёСЃР»РµРЅРёРµ РѕС€РёР±РєРё РёРЅС‚РµСЂРїРѕР»СЏС†РёРё РґР»СЏ 3, 4 РёР»Рё 5 СѓР·Р»РѕРІ РёРЅС‚РµСЂРїРѕР»СЏС†РёРё
+// РЅР° РїРѕСЃР»РµРґРѕРІР°С‚РµР»СЊРЅРѕСЃС‚Рё РѕС‚СЂРµР·РєРѕРІ [0,2], [0,1], [0,1/2], ... [0, 1/16].
+// Р§РµР±С‹С€РµРІСЃРєРѕРµ СЂР°СЃРїРѕР»РѕР¶РµРЅРёРµ СѓР·Р»РѕРІ.
     Cheb_nodes = true;
     bt = b;
     for (int bk = 0; bk<6; bk++){
         err = Newton_interpolator_err<double, double, 3>(a, bt, Cheb_nodes);
-        std::cout << "3" << ";" << bt << ";" << err << ";" << "Чеб" << std::endl;
+        std::cout << "3" << ";" << bt << ";" << err << ";" << "Р§РµР±" << std::endl;
         bt = bt/2.0;
     }
     bt = b;
     for (int bk = 0; bk<6; bk++){
         err = Newton_interpolator_err<double, double, 4>(a, bt, Cheb_nodes);
-        std::cout << "4" << ";" << bt << ";" << err << ";" << "Чеб" << std::endl;
+        std::cout << "4" << ";" << bt << ";" << err << ";" << "Р§РµР±" << std::endl;
         bt = bt/2.0;
     }
     bt = b;
     for (int bk = 0; bk<6; bk++){
         err = Newton_interpolator_err<double, double, 5>(a, bt, Cheb_nodes);
-        std::cout << "5" << ";" << bt << ";" << err << ";" << "Чеб" << std::endl;
+        std::cout << "5" << ";" << bt << ";" << err << ";" << "Р§РµР±" << std::endl;
         bt = bt/2.0;
     }
 
